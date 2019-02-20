754cab7c8608b8a7d078580b114ee04d5251f11e
Jordi Puiggené <jp@naralabs.com>
2019-02-20 23:34:48 +0100

    Improvements in ListingAdapter for stored state

--- src/senaite/storage/browser/analysisrequests.py
+++ src/senaite/storage/browser/analysisrequests.py
@@ -1,29 +1,61 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.STORAGE
+#
+# Copyright 2019 by it's authors.
+#
+
 from senaite.core.listing import utils
 from senaite.core.listing.interfaces import IListingView, IListingViewAdapter
+from senaite.storage import api
 from senaite.storage import senaiteMessageFactory as _
 from zope.component import adapts
 from zope.interface import implements
-from bika.lims import api
 
 
 class AnalysisRequestsListingViewAdapter(object):
     adapts(IListingView)
     implements(IListingViewAdapter)
 
+    # Order of priority of this subscriber adapter over others
+    priority_order = 10
+
     def __init__(self, listing, context):
         self.listing = listing
         self.context = context
 
     def before_render(self):
-        # Add stored review state
+        # Add review state "stored" in the listing
+        self.add_stored_review_state()
+
+        # In "stored" status, display all samples in "flat style"
+        if self.is_stored_state():
+            if "isRootAncestor" in self.listing.contentFilter:
+                del self.listing.contentFilter["isRootAncestor"]
+
+    def add_stored_review_state(self):
+        """Adds the "stored" review state to the listing's review_states pool
+        """
+        # Columns to hide
+        hide = ["getAnalysesNum",
+                "getDateVerified",
+                "getDatePreserved",
+                "getDatePublished",
+                "getDueDate",
+                "getStorageLocation",
+                "Printed"
+                "Progress",
+                "SamplingDate",]
+        columns = filter(lambda c: c not in hide, self.listing.columns.keys())
+
+        # Custom transitions
         print_stickers = {
             "id": "print_stickers",
             "title": _("Print stickers"),
             "url": "workflow_action?action=print_stickers"
         }
-        base_column_ids = self.listing.columns.keys()
-        hide = ["getDateVerified", "getDatePublished"]
-        columns = filter(lambda col: col not in hide, base_column_ids)
+
+        # "stored" review state
         stored = {
             "id": "stored",
             "title": _("Stored"),
@@ -36,13 +68,14 @@ class AnalysisRequestsListingViewAdapter(object):
             "custom_transitions": [print_stickers],
             "columns": columns,
         }
+
+        # Add the review state
         utils.add_review_state(self.listing, stored, after="published")
 
-        # Add the column Date Stored to the "stored" review state
+        # Add the column "DateStored" to "stored" review_state
         column_values = {
             "title": _("Date stored"),
             "index": "getDateStored",
-            "attr": "getId",
             "toggle": True}
         utils.add_column(self.listing, "getDateStored", column_values,
                          after="getDateReceived", review_states=("stored",))
@@ -60,10 +93,27 @@ class AnalysisRequestsListingViewAdapter(object):
                              after="getDateStored", review_states=("stored", ))
 
     def folder_item(self, obj, item, index):
-        listing = self.listing
-        item["getDateStored"] = ""
-        date_stored = obj.getDateStored
-        if date_stored:
-            item["getDateStored"] = listing.ulocalized_time(date_stored,
-                                                            long_format=1)
+        # Do nothing if the current state is not "stored"
+        if not self.is_stored_state():
+            return item
+
+        # Display all samples in "flat style"
+        item["parent"] = ""
+        item["children"] = []
+
+        # Show the date time when the sample was stored
+        item["getDateStored"] = self.str_time(obj.getDateStored)
         return item
+
+    def str_time(self, date_time, long_format=1):
+        """Returns a string representation of localized DateTime
+        """
+        return self.listing.ulocalized_time(date_time, long_format=long_format)
+
+    def is_stored_state(self):
+        """Returns whether the current review state of the listing is "stored"
+        """
+        review_state = self.listing.review_state
+        if not review_state:
+            return False
+        return review_state.get("id","") == "stored"
