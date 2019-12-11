2d7858121456d8202cd5c1a1014efae344065296
Ramon Bartl <rb@ridingbytes.com>
2019-12-11 09:44:30 +0100

    Changed Base Catalog Tool
    See: https://github.com/senaite/senaite.core/pull/1487

--- src/senaite/storage/catalog.py
+++ src/senaite/storage/catalog.py
@@ -18,22 +18,22 @@
 # Copyright 2019 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
-from AccessControl import ClassSecurityInfo
 from App.class_init import InitializeClass
-from bika.lims.catalog.bika_catalog_tool import BikaCatalogTool
+from bika.lims.catalog.base import BaseCatalog
 from senaite.storage.interfaces import ISenaiteStorageCatalog
 from zope.interface import implements
 
 SENAITE_STORAGE_CATALOG = "senaite_storage_catalog"
 
-class SenaiteStorageCatalog(BikaCatalogTool):
+
+class SenaiteStorageCatalog(BaseCatalog):
     implements(ISenaiteStorageCatalog)
-    security = ClassSecurityInfo()
 
     def __init__(self):
-        BikaCatalogTool.__init__(self,
-                                 id=SENAITE_STORAGE_CATALOG,
-                                 title='Senaite storage catalog',
-                                 portal_meta_type='SenaiteStorageCatalog')
+        BaseCatalog.__init__(self,
+                             id=SENAITE_STORAGE_CATALOG,
+                             title="Senaite storage catalog",
+                             portal_meta_type="SenaiteStorageCatalog")
+
 
 InitializeClass(SenaiteStorageCatalog)
