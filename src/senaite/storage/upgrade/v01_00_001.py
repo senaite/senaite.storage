21227178b2218c4f50e1e9a128b4953987717a2c
Jordi Puiggené <jp@naralabs.com>
2020-02-11 13:53:30 +0100

    Wrong code headers

--- src/senaite/storage/upgrade/v01_00_001.py
+++ src/senaite/storage/upgrade/v01_00_001.py
@@ -2,8 +2,8 @@
 #
 # This file is part of SENAITE.STORAGE.
 #
-# SENAITE.STORAGE is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
+# SENAITE.STORAGE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free
 # Software Foundation, version 2.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
