59bd19a001764bac7ad71967227d59bded820edc
Ramon Bartl <rb@ridingbytes.com>
2021-01-18 23:39:37 +0100

    Fixed content filter for storage position listing

--- src/senaite/storage/browser/position/view.py
+++ src/senaite/storage/browser/position/view.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from bika.lims import api
 from senaite.storage.browser.facility.view import FacilityListingView
 
 
@@ -9,6 +10,19 @@ class PositionListingView(FacilityListingView):
 
     def __init__(self, context, request):
         super(PositionListingView, self).__init__(context, request)
+
+        self.contentFilter = {
+            "portal_type": [
+                "StorageContainer",
+                "StorageSamplesContainer",
+            ],
+            "sort_on": "sortable_title",
+            "sort_order": "ascending",
+            "path": {
+                "query": api.get_path(context),
+                "depth": 1,
+            }
+        }
 
         self.icon = "{}/{}".format(
             self.portal_url, "senaite_theme/icon/storage-position")
