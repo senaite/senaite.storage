0f06dfd2c714916c389f1a58790f00ed5e863b5b
Ramon Bartl <rb@ridingbytes.com>
2021-01-24 11:23:31 +0100

    book out samples view implemented

---
+++ src/senaite/storage/browser/container/book_out_samples.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+from bika.lims import api
+from bika.lims.interfaces import IAnalysisRequest
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from senaite.storage import logger
+from senaite.storage.interfaces import IStorageSamplesContainer
+from senaite.storage import senaiteMessageFactory as _
+from senaite.storage.browser import BaseView
+
+
+class BookOutSamplesView(BaseView):
+    """Store Container View. Allows to store samples in preselected containers
+    """
+    template = ViewPageTemplateFile("templates/book_out_samples.pt")
+    action = "storage_book_out_samples"
+
+    def __init__(self, context, request):
+        super(BookOutSamplesView, self).__init__(context, request)
+        self.context = context
+        self.request = request
+        self.portal = api.get_portal()
+        self.back_url = self.context.absolute_url()
+
+    def __call__(self):
+        form = self.request.form
+
+        # Form submit toggle
+        form_submitted = form.get("submitted", False)
+        form_move = form.get("button_book_out", False)
+        form_cancel = form.get("button_cancel", False)
+
+        # Handle book out
+        if form_submitted and form_move:
+            logger.info("*** BOOK OUT ***")
+            uids = form.get("uids", [])
+            comment = form.get("comment", "")
+            samples = map(api.get_object_by_uid, uids)
+            wf = api.get_tool("portal_workflow")
+            for sample in samples:
+                wf.doActionFor(sample, "book_out", comment=comment)
+            return self.redirect()
+
+        # Handle cancel
+        if form_submitted and form_cancel:
+            return self.redirect(message=_("Cancelled"))
+        return self.template()
+
+    def get_objects(self):
+        """Get the objects to be moved
+        """
+        if IAnalysisRequest.providedBy(self.context):
+            return [self.context]
+
+        # fetch objects from request
+        objs = self.get_objects_from_request()
+        samples = []
+        for obj in objs:
+            if IStorageSamplesContainer.providedBy(obj):
+                samples.extend(obj.get_samples())
+
+        return samples
+
+    def get_title(self, obj):
+        """Return the object title as unicode
+        """
+        title = api.get_title(obj)
+        return api.safe_unicode(title)
+
+    def get_samples_data(self):
+        """Returns a list of containers that can be moved
+        """
+        for obj in self.get_objects():
+            obj = api.get_object(obj)
+            yield {
+                "obj": obj,
+                "id": api.get_id(obj),
+                "uid": api.get_uid(obj),
+                "title": self.get_title(obj),
+                "url": api.get_url(obj),
+            }
