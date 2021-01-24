ba2a2346615f489a1bf8dc49c5c46f57bc8e3cc3
Ramon Bartl <rb@ridingbytes.com>
2021-01-24 15:29:24 +0100

    Skip guard checks

--- src/senaite/storage/workflow/sample/guards.py
+++ src/senaite/storage/workflow/sample/guards.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from AccessControl.SecurityInfo import ModuleSecurityInfo
-from bika.lims import api
 
 security = ModuleSecurityInfo(__name__)
 
@@ -9,19 +8,15 @@ security = ModuleSecurityInfo(__name__)
 @security.public
 def guard_store_sample(sample):
     """Sample guard to control if the sample can be stored
-
-    Only possible when all analyses are readonly
     """
-    return not has_editable_analyses(sample)
+    return True
 
 
 @security.public
 def guard_book_out_sample(sample):
     """Sample guard to control if the sample can be booked out
-
-    Only possible when all analyses are readonly
     """
-    return not has_editable_analyses(sample)
+    return True
 
 
 @security.public
@@ -29,18 +24,3 @@ def guard_recover_sample(sample):
     """Sample guard to control if the sample can be recovered
     """
     return True
-
-
-def has_editable_analyses(sample):
-    """Checks if the sample contains editable analyses
-
-    This ensures there are no analyses where fields are editable.
-    """
-    analyses = sample.getAnalyses()
-    if not analyses:
-        return False
-    for analysis in analyses:
-        status = api.get_workflow_status_of(analysis)
-        if status in ["assigned", "unassigned"]:
-            return True
-    return False
