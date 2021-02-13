bd534b58ed1d2fbdd439e770fde4000b946e2bc8
Ramon Bartl <rb@ridingbytes.com>
2021-02-13 12:14:33 +0100

    Check if senaite.storage is installed

--- src/senaite/storage/subscribers/upgrade.py
+++ src/senaite/storage/subscribers/upgrade.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from senaite.storage import is_installed
+from senaite.storage import logger
 from senaite.storage.setuphandlers import post_install
 
 
 def afterUpgradeStepHandler(event):
     """Event handler that is executed after running an upgrade step of senaite.core
     """
+    if not is_installed():
+        return
+    logger.info("Run senaite.storage.afterUpgradeStepHandler ...")
     setup = event.context
     post_install(setup)
+    logger.info("Run senaite.storage.afterUpgradeStepHandler [DONE]")
