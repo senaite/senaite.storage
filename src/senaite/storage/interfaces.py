fc76a2f0850ceec3a0137d46fa13d23a32373cc7
Ramon Bartl <rb@ridingbytes.com>
2021-11-11 10:46:45 +0100

    Let storage catalog interface inherit from ISenaiteCatalog

--- src/senaite/storage/interfaces.py
+++ src/senaite/storage/interfaces.py
@@ -19,6 +19,7 @@
 # Some rights reserved, see README and LICENSE.
 
 from bika.lims.interfaces import IBikaLIMS
+from senaite.core.interfaces import ISenaiteCatalog
 from senaite.lims.interfaces import ISenaiteLIMS
 from zope.interface import Interface
 from zope.viewlet.interfaces import IViewletManager
@@ -32,7 +33,7 @@ class ISenaiteStorageLayer(IBikaLIMS, ISenaiteLIMS):
     """
 
 
-class ISenaiteStorageCatalog(Interface):
+class ISenaiteStorageCatalog(ISenaiteCatalog):
     """Marker interface for senaite_storage_catalog CatalogTool
     """
 
