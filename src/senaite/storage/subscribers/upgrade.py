82f5964e05d04c3ee82f666696515e99f9794bae
Jordi Puiggené <jp@naralabs.com>
2025-12-30 19:15:29 +0100

    Fix ValueError: undefined property 'add_permission' on upgrade
    ```
    Traceback (innermost last):
      Module ZPublisher.WSGIPublisher, line 176, in transaction_pubevents
      Module ZPublisher.WSGIPublisher, line 385, in publish_module
      Module ZPublisher.WSGIPublisher, line 288, in publish
      Module ZPublisher.mapply, line 85, in mapply
      Module ZPublisher.WSGIPublisher, line 63, in call_object
      Module Products.CMFPlone.controlpanel.browser.quickinstaller, line 666, in __call__
      Module Products.CMFPlone.controlpanel.browser.quickinstaller, line 399, in upgrade_product
      Module Products.GenericSetup.tool, line 1202, in upgradeProfile
      Module Products.GenericSetup.upgrade, line 185, in doStep
      Module senaite.core.upgrade, line 41, in wrap_func_args
      Module zope.event, line 33, in notify
      Module zope.component.event, line 27, in dispatch
      Module zope.component._api, line 134, in subscribers
      Module zope.interface.registry, line 448, in subscribers
      Module zope.interface.adapter, line 899, in subscribers
      Module senaite.storage.subscribers.upgrade, line 40, in afterUpgradeStepHandler
      Module Products.GenericSetup.tool, line 375, in runImportStepFromProfile
      Module Products.GenericSetup.tool, line 1323, in _doRunImportStep
       - __traceback_info__: typeinfo
      Module Products.CMFCore.exportimport.typeinfo, line 222, in importTypesTool
      Module Products.GenericSetup.utils, line 933, in importObjects
       - __traceback_info__: portal_types
      Module Products.GenericSetup.utils, line 929, in importObjects
       - __traceback_info__: types/StorageRootFolder
      Module Products.GenericSetup.utils, line 530, in _importBody
      Module Products.CMFCore.exportimport.typeinfo, line 61, in _importNode
      Module Products.GenericSetup.utils, line 762, in _initProperties
    ValueError: undefined property 'add_permissio
    ```

---
+++ src/senaite/storage/subscribers/upgrade.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.STORAGE.
+#
+# SENAITE.STORAGE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2019-2024 by it's authors.
+# Some rights reserved, see README and LICENSE.
+
+from bika.lims import api
+from bika.lims.api import get_portal
+from plone.dexterity.fti import DexterityFTI
+from senaite.storage import is_installed
+from senaite.storage import logger
+from senaite.storage import PRODUCT_NAME
+from senaite.storage.setuphandlers import setup_catalogs
+from senaite.storage.setuphandlers import setup_workflows
+
+
+def afterUpgradeStepHandler(event):
+    """Event handler that is executed after running an upgrade step of senaite.core
+    """
+    if not is_installed():
+        return
+
+    logger.info("Run {}.afterUpgradeStepHandler ...".format(PRODUCT_NAME))
+    portal = get_portal()
+    setup = portal.portal_setup  # noqa
+
+    profile = "profile-{0}:default".format(PRODUCT_NAME)
+
+    # Only run typeinfo import if all types are already migrated to DX
+    # to avoid errors when trying to apply DX properties to AT types
+    if types_migrated_to_dx(portal):
+        setup.runImportStepFromProfile(profile, "typeinfo")
+    else:
+        logger.info("Skipping typeinfo import - AT to DX migration pending")
+
+    setup.runImportStepFromProfile(profile, "rolemap")
+    setup.runImportStepFromProfile(profile, "workflow")
+
+    # Setup catalogs
+    setup_catalogs(portal)
+
+    # Setup workflows
+    setup_workflows(portal)
+
+    logger.info("Run {}.afterUpgradeStepHandler [DONE]".format(PRODUCT_NAME))
+
+
+def types_migrated_to_dx(portal):
+    """Check if all storage types have been migrated to Dexterity
+    """
+    pt = api.get_tool("portal_types")
+    storage_types = [
+        "StorageRootFolder",
+        "StorageFacility",
+        "StorageContainer",
+        "StorageSamplesContainer",
+    ]
+
+    for type_name in storage_types:
+        fti = pt.getTypeInfo(type_name)
+        if not fti:
+            # Type doesn't exist yet - not migrated
+            return False
+        if not isinstance(fti, DexterityFTI):
+            # Still an AT type - not migrated
+            logger.info("Type '{}' is still AT, migration needed".format(type_name))
+            return False
+
+    return True
