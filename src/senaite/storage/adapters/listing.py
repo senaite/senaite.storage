5111ee64cc82e751f700e33095e60980cc995871
Jordi Puiggené <jp@naralabs.com>
2026-03-07 06:24:06 +0100

    Add a "Past retention" filter in samples listing
    Samples their storage expiry date have passed are displayed in
    the listing when "Past retention" button is clicked

--- src/senaite/storage/adapters/listing.py
+++ src/senaite/storage/adapters/listing.py
@@ -18,20 +18,113 @@
 # Copyright 2019-2024 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
+import copy
+
 from bika.lims import api
+from bika.lims import senaiteMessageFactory as _s
 from senaite.app.listing import utils
 from senaite.app.listing.interfaces import IListingView
 from senaite.app.listing.interfaces import IListingViewAdapter
+from senaite.core.api import dtime
 from senaite.storage import is_installed
 from senaite.storage import senaiteMessageFactory as _
-from zope.component import adapts
+from zope.component import adapter
 from zope.interface import implementer
-from senaite.core.api import dtime
+
+HIDE_COLUMNS = (
+    "getAnalysesNum",
+    "getDateVerified",
+    "getDatePreserved",
+    "getDatePublished",
+    "getDueDate",
+    "getStorageLocation",
+    "Printed"
+    "Progress",
+    "SamplingDate",
+)
+
+ADD_COLUMNS = (
+    {
+        "id": "getDateStored",
+        "title": _(
+            u"listing_samples_column_date_stored",
+            default=u"Date stored"
+        ),
+        "index": "getDateStored",
+        "toggle": True,
+        "after": "getDateReceived",
+        "review_states": ("stored", "past_retention"),
+    },
+    {
+        "id": "getStorageExpiryDate",
+        "title": _(
+            u"listing_samples_column_storage_expiry_date",
+            default=u"Storage expiry date"
+        ),
+        "toggle": True,
+        "after": "getDateStored",
+        "review_states": ("stored", "past_retention"),
+    },
+    {
+        "id": "getSamplesContainer",
+        "title": _(
+            u"listing_samples_column_storage",
+            default=u"Storage"
+        ),
+        "attr": "getSamplesContainerID",
+        "replace_url": "getSamplesContainerURL",
+        "toggle": True,
+        "after": "getDateStored",
+        "review_states": ("stored", "past_retention"),
+        "condition": "is_lab_user",
+    }
+)
+
+ADD_CUSTOM_TRANSITIONS = (
+    {
+        "id": "print_stickers",
+        "title": _s("Print stickers"),
+        "url": "workflow_action?action=print_stickers",
+        "review_states": ("stored", "past_retention")
+    },
+)
+
+ADD_REVIEW_STATES = (
+    {
+        "id": "stored",
+        "title": _(
+            u"listing_samples_state_stored",
+            default=u"Stored"
+        ),
+        "contentFilter": {
+            "review_state": ("stored",),
+            "sort_on": "created",
+            "sort_order": "descending",
+        },
+        "confirm_transitions": ["recover"],
+        "after": "published",
+    },
+    {
+        "id": "past_retention",
+        "title": _(
+            u"listing_samples_state_past_retention",
+            default=u"Past Retention"
+        ),
+        "contentFilter": {
+            "review_state": ("stored",),
+            "sort_on": "getDateStored",
+            "sort_order": "descending",
+        },
+        "transitions": [],
+        "confirm_transitions": ["recover"],
+        "after": "stored",
+    },
+)
 
 
+@adapter(IListingView)
 @implementer(IListingViewAdapter)
 class AnalysisRequestsListingViewAdapter(object):
-    adapts(IListingView)
 
     # Order of priority of this subscriber adapter over others
     priority_order = 10
@@ -47,88 +140,68 @@ class AnalysisRequestsListingViewAdapter(object):
         if not self.installed:
             return
 
-        # Add review state "stored" in the listing
-        self.add_stored_review_state()
+        # Additional filters/review statuses
+        map(self.add_review_state, ADD_REVIEW_STATES)
+
+        # Additional columns
+        map(self.add_column, ADD_COLUMNS)
+
+        # Additional custom transitions
+        map(self.add_custom_transition, ADD_CUSTOM_TRANSITIONS)
 
         # In "stored" status, display all samples in "flat style"
         if self.is_stored_state():
             self.flat_listing = True
             self.listing.contentFilter.pop("isRootAncestor", None)
 
-    def add_stored_review_state(self):
-        """Adds the "stored" review state to the listing's review_states pool
+    def add_review_state(self, state_info):
+        """Adds the review state with the provided information
         """
-        # Columns to hide
-        hide = [
-            "getAnalysesNum",
-            "getDateVerified",
-            "getDatePreserved",
-            "getDatePublished",
-            "getDueDate",
-            "getStorageLocation",
-            "Printed"
-            "Progress",
-            "SamplingDate",
-        ]
-        columns = filter(lambda c: c not in hide, self.listing.columns.keys())
+        info = copy.deepcopy(state_info)
+        after = info.pop("after", None)
+        cols = info.pop("columns", None)
+        if not cols:
+            cols = self.listing.columns.keys()
+            cols = list(filter(lambda col: col not in HIDE_COLUMNS, cols))
+        info["columns"] = cols
+        utils.add_review_state(self.listing, info, after=after)
 
-        # Custom transitions
-        print_stickers = {
-            "id": "print_stickers",
-            "title": _("Print stickers"),
-            "url": "workflow_action?action=print_stickers"
-        }
+    def add_column(self, column_info):
+        """Adds the column with the provided information
+        """
+        info = copy.deepcopy(column_info)
+        condition = info.pop("condition", None)
+        if condition and not getattr(self, condition)():
+            # do not add the column if condition is not met
+            return
 
-        # "stored" review state
-        stored = {
-            "id": "stored",
-            "title": _("Stored"),
-            "contentFilter": {
-                "review_state": ("stored",),
-                "sort_on": "created",
-                "sort_order": "descending",
-            },
-            "transitions": [],
-            "custom_transitions": [print_stickers],
-            "confirm_transitions": ["recover"],
-            "columns": columns,
-        }
+        column_id = info.pop("id")
+        after = info.pop("after", None)
+        review_states = info.pop("review_states", None)
+        if review_states is None:
+            review_states = [st["id"] for st in self.listing.review_states]
 
-        # Add the review state
-        utils.add_review_state(self.listing, stored, after="published")
+        utils.add_column(listing=self.listing,
+                         column_id=column_id,
+                         column_values=info,
+                         after=after,
+                         review_states=review_states)
 
-        # Add the column "DateStored" to "stored" review_state
-        column_values = {
-            "title": _("Date stored"),
-            "index": "getDateStored",
-            "toggle": True}
-        utils.add_column(self.listing, "getDateStored", column_values,
-                         after="getDateReceived", review_states=("stored",))
+    def add_custom_transition(self, transition_info):
+        """Adds a custom transition for the given statuses
+        """
+        info = copy.deepcopy(transition_info)
+        statuses = info.pop("review_states")
+        rss = filter(lambda a: a["id"] in statuses, self.listing.review_states)
+        for rs in rss:
+            rs.setdefault("custom_transitions", []).append(info)
 
-        # Add Samples Container column, but only if the current user logged in
-        # is not a client contact
-        if not api.get_current_client():
-            column_values = {
-                "title": _("Storage"),
-                "attr": "getSamplesContainerID",
-                "replace_url": "getSamplesContainerURL",
-                "toggle": True
-            }
-            utils.add_column(self.listing, "getSamplesContainer",
-                             column_values, after="getDateStored",
-                             review_states=("stored", ))
-
-        # Add expiry date column
-        utils.add_column(
-            self.listing,
-            column_id="getStorageExpiryDate",
-            column_values=dict(
-                title=_("Storage Expiry Date"),
-                toggle=True,
-            ),
-            after="getDateStored",
-            review_states=("stored", )
-        )
+    def is_lab_user(self):
+        """Returns whether the current user is not from a client
+        """
+        if api.get_current_client():
+            return False
+        return True
 
     def folder_item(self, obj, item, index):
         # Return immediately when add-on is not installed
@@ -162,4 +235,5 @@ class AnalysisRequestsListingViewAdapter(object):
         review_state = self.listing.review_state
         if not review_state:
             return False
-        return review_state.get("id", "") == "stored"
+        statuses = [st["id"] for st in ADD_REVIEW_STATES]
+        return review_state.get("id", "") in statuses
